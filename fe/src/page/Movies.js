ef0ce5e2a73ba8cfd5a896fc903cb944188eda2e
shanghanrun <idim7@naver.com>
2024-06-02 17:02:57 +0900

    window.location.href 대신에 navigate 사용

--- fe/src/page/Movies.js
+++ fe/src/page/Movies.js
@@ -1,19 +1,24 @@
 import { useEffect } from 'react';
 import axios from 'axios'
 import userStore from '../store/userStore'
+import { useNavigate } from 'react-router-dom';
 
 const Movies =()=>{
+	const navigate = useNavigate();
 	const {user} = userStore()
 	let url
 	useEffect(()=>{
 		if(user){
-			let userInfo =user
-			userInfo = userInfo.JSON.stringify()
-			url = `https://eloquent-pastelito-120fb6.netlify.app?user=${userInfo}`;
-			window.location.href = url;
+			let userInfo = JSON.stringify(user)
+			navigate(`https://eloquent-pastelito-120fb6.netlify.app?user=${encodeURIComponent(userInfo)}`);
+			// let userInfo =user
+			// userInfo = userInfo.JSON.stringify()
+			// url = `https://eloquent-pastelito-120fb6.netlify.app?user=${userInfo}`;
+			// window.location.href = url;
 		} else{
 			url ='https://eloquent-pastelito-120fb6.netlify.app'
-			window.location.href = url;
+			 navigate('https://eloquent-pastelito-120fb6.netlify.app');
+			// window.location.href = url;
 		}
 		// const token = sessionStorage.getItem('token'); // 예: 로컬 스토리지 또는 세션 스토리지에서 가져옴
 		// const url = `https://eloquent-pastelito-120fb6.netlify.app?token=${token}`;
