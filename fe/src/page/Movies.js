8575e71d4b7f6a72a15e9806ff0a3610106a3f21
shanghanrun <idim7@naver.com>
2024-06-02 16:38:01 +0900

    neflix로 이동할 때 token을 넘기지 않고 user를 넘기는 걸로 바꾸었다.

--- fe/src/page/Movies.js
+++ fe/src/page/Movies.js
@@ -1,12 +1,22 @@
 import { useEffect } from 'react';
 import axios from 'axios'
+import userStore from '../store/userStore'
 
 const Movies =()=>{
+	const {user} = userStore()
 
 	useEffect(()=>{
-		const token = sessionStorage.getItem('token'); // 예: 로컬 스토리지 또는 세션 스토리지에서 가져옴
-		const url = `https://eloquent-pastelito-120fb6.netlify.app?token=${token}`;
-		window.location.href = url;
+		if(user){
+			let userInfo =user
+			userInfo = userInfo.JSON.stringify()
+			const url = `https://eloquent-pastelito-120fb6.netlify.app?user=${userInfo}`;
+			window.location.href = url;
+		} else{
+			const url ='https://eloquent-pastelito-120fb6.netlify.app'
+		}
+		// const token = sessionStorage.getItem('token'); // 예: 로컬 스토리지 또는 세션 스토리지에서 가져옴
+		// const url = `https://eloquent-pastelito-120fb6.netlify.app?token=${token}`;
+		// window.location.href = url;
 
 		// window.location.href = 'https://eloquent-pastelito-120fb6.netlify.app';
 		// const movie = axios.get('https://eloquent-pastelito-120fb6.netlify.app')
