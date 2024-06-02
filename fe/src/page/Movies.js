4a508ff44c6f9567f69d6d6b891f86324d067026
shanghanrun <idim7@naver.com>
2024-06-02 16:39:38 +0900

    .

--- fe/src/page/Movies.js
+++ fe/src/page/Movies.js
@@ -4,15 +4,16 @@ import userStore from '../store/userStore'
 
 const Movies =()=>{
 	const {user} = userStore()
-
+	let url
 	useEffect(()=>{
 		if(user){
 			let userInfo =user
 			userInfo = userInfo.JSON.stringify()
-			const url = `https://eloquent-pastelito-120fb6.netlify.app?user=${userInfo}`;
+			url = `https://eloquent-pastelito-120fb6.netlify.app?user=${userInfo}`;
 			window.location.href = url;
 		} else{
-			const url ='https://eloquent-pastelito-120fb6.netlify.app'
+			url ='https://eloquent-pastelito-120fb6.netlify.app'
+			window.location.href = url;
 		}
 		// const token = sessionStorage.getItem('token'); // 예: 로컬 스토리지 또는 세션 스토리지에서 가져옴
 		// const url = `https://eloquent-pastelito-120fb6.netlify.app?token=${token}`;
